69890fb43d993341dc7fb7edee46a83374955731
Iona Buchanan <iona.buchanan10@gmail.com>
2021-05-10 12:15:14 -0400

    add cam stuff

--- soccer_bot_ctrl.py
+++ soccer_bot_ctrl.py
@@ -1,10 +1,10 @@
 import sys
-import picamera
+#import picamera
 import pygame
 import io
-import pygame.camera
+#import pygame.camera
 pygame.init()
-pygame.camera.init()
+#pygame.camera.init()
 
 #screen = pygame.display.set_mode((640,480),0)
 screen = pygame.display.set_mode((0,0))
@@ -14,13 +14,15 @@ cam_list = pygame.camera.list_cameras()
 cam = pygame.camera.Camera(cam_list[0],(32,24))
 cam.start()
 '''
-
+'''
 camera = picamera.PiCamera()
 camera.resolution = (1280, 720)
 camera.crop = (0.0, 0.0, 1.0, 1.0)
 
 x = (screen.get_width() - camera.resolution[0]) / 2
 y = (screen.get_height() - camera.resolution[1]) / 2
+rgb = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
+'''
 
 while True:
 
@@ -33,6 +35,7 @@ while True:
     screen.blit(image1,(0,0))
     '''
 
+    '''
     stream = io.BytesIO()
     camera.capture(stream, use_video_port=True, format='rgb')
     stream.seek(0)
@@ -41,6 +44,8 @@ while True:
     img = pygame.image.frombuffer(rgb[0:
           (camera.resolution[0] * camera.resolution[1] * 3)],
            camera.resolution, 'RGB')
+    '''
+    img = None
     if img:
         screen.blit(img, (x,y))
 
@@ -60,27 +65,3 @@ while True:
                 location -= 1
             if event.key == pygame.K_RIGHT:
                 location += 1
-
-'''
-
-
-
-
-
-
-# Init buffer
-rgb = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
-
-# Main loop
-exitFlag = True
-while(exitFlag):
-    for event in pygame.event.get():
-        if(event.type is pygame.MOUSEBUTTONDOWN or 
-           event.type is pygame.QUIT):
-            exitFlag = False
-
-
-
-
-
-'''
